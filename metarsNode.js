da8ad2cb9b0629f5dca95718d30d7b5e1b321e03
rifflesbits <rifflesbits@users.noreply.github.com>
2019-07-19 09:48:44 -0400

    updated var names

--- metarsNode.js
+++ metarsNode.js
@@ -1,8 +1,8 @@
 /*
-  Simple application to get weather metars from airport ids
+  Simple web application to get FAA weather metars from provided airport ids
 */
 const express = require('express');
-const app = express();
+const expressApp = express();
 const request = require('request');
 const DOMParser = require('xmldom').DOMParser;
 const mustache = require('mustache');
@@ -11,8 +11,8 @@ const mustache = require('mustache');
 // the http request sent to our server functions here
 const bodyParser = require('body-parser');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+expressApp.use(bodyParser.json());
+expressApp.use(bodyParser.urlencoded({extended: true}));
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -35,52 +35,56 @@ function getWx(airportIds, httpResponse){
 
     var wxDoc = new DOMParser().parseFromString(body);
 
+    // array of xml element objects themselves (not the direct string values)
     var aWxReportsRawTxtEls = wxDoc.getElementsByTagName('raw_text');
 
-
+    // array to store the actual string values of the wx reports
     var aAllReports = [];
 
     // parse the xml dom elements; get the string values out of them
     for(var i=0; i < aWxReportsRawTxtEls.length; i++){
 
+      // just the element object
       var firstWxReportEl = aWxReportsRawTxtEls[i];
 
-      var firstWxReportTxtEl = firstWxReportEl.childNodes[0];
+      // the text node contained by the element object
+      var firstWxReportTxtNode = firstWxReportEl.childNodes[0];
 
-      var firstWxReportTxtVal = firstWxReportTxtEl.nodeValue;
+      // the actual string value of the txt node
+      var sFirstWxReportTxtVal = firstWxReportTxtNode.nodeValue;
 
-      aAllReports.push(firstWxReportTxtVal);
+      aAllReports.push(sFirstWxReportTxtVal);
     }
 
     console.log('aAllReports: ' + aAllReports);
 
     // prepare object holding array of wx report results that we can send to the 
     // mustache template engine
-    var view = {
+    var viewContainer = {
       wxReports: aAllReports
     };
 
     // create a string with mustache template putting each wx report from array on a new line
-    var output = mustache.render('{{#wxReports}} <p> {{.}} </p> {{/wxReports}} ', view);
+    var wxReportsHtml = mustache.render('{{#wxReports}} <p> {{.}} </p> {{/wxReports}} ', viewContainer);
 
-    console.log('output: \n' + output);
+    console.log('output: \n' + wxReportsHtml);
 
     httpResponse.writeHead(200, {'Content-Type': 'text/html'});
-    httpResponse.write(output);
+    httpResponse.write(wxReportsHtml);
     httpResponse.end();
   });
 }
 
-// configure our ejs view resolver engine
-app.set('views', './views');
-app.set('view engine', 'ejs');
+// configure express with our ejs view resolver engine
+expressApp.set('views', './views');
+expressApp.set('view engine', 'ejs');
 
 /*
   Show the initial load page that has the
   text box entry for airport ids and the
   submit button
 */
-app.get('/', (req, res) => {
+expressApp.get('/', (req, res) => {
   res.render('index');
 });
 
@@ -88,7 +92,7 @@ app.get('/', (req, res) => {
   Handles request to get wx wxReports
   Depends on input json containing req param: airportIds
 */
-app.post('/wx', function(req, res){
+expressApp.post('/wx', function(req, res){
 
   var airportIds = req.body.airportIds;
 
@@ -97,7 +101,7 @@ app.post('/wx', function(req, res){
   getWx(airportIds, res);
 });
 
-// entry point for starting the app server
-app.listen(port, () => {
-  console.log('App Listening! on port:' + port)
+// entry point for starting the expressApp server
+expressApp.listen(port, () => {
+  console.log('Metars webapp running! on port: ' + port)
 });
